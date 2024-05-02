6ff68c45a36932179cedabbf3f4a415903741881
imran <mdimrankhan175@gmail.com>
2024-05-02 06:36:46 +0530

    summary page updated

--- src/components/SummaryPage/index.js
+++ src/components/SummaryPage/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import styled from 'styled-components';
+
+const SummaryPageContainer = styled.div`
+  border: 2px solid blue;
+  padding: 10px;
+  margin: 20px;  
+  border-radius:10px;
+  width:30%;
+`;     
+
+
+const Heading=styled.h3`
+font-family:'roboto';
+color:green;
+font-weight:800;
+`;
 
 const SummaryPage = ({ statusList }) => {
   return (
-    <BarChart width={200} height={300} data={statusList}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="task" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="percentage" fill="#8884d8" />
-    </BarChart>
+    <SummaryPageContainer>
+      <Heading>Task Summary Page</Heading>
+      <BarChart width={300} height={300} data={statusList}>       
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="task" />
+        <YAxis type="number" domain={[0, 100]} />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+        <Bar dataKey="percentage" fill="#8884d8" />
+      </BarChart>
+    </SummaryPageContainer>
   );
+};
+
+// Custom Tooltip component to display task name and percentage
+const CustomTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div style={{ backgroundColor: 'white', padding: '5px', border: '1px solid #ccc' }}>
+        <p>{`Task: ${payload[0].payload.task}`}</p>
+        <p>{`Percentage: ${payload[0].value}%`}</p> {/* Added % symbol */}
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default SummaryPage;
